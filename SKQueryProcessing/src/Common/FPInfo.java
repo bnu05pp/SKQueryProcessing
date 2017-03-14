2d9a0bdf8e6e3b5c17134b9d72478b220f916914
Peng Peng <bnu05pp@gmail.com>
2017-03-14 11:39:32 +0800

    version 1.0

--- SKQueryProcessing/src/Common/FPInfo.java
+++ SKQueryProcessing/src/Common/FPInfo.java
@@ -1,34 +1,57 @@
 package Common;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.TreeSet;
 
 public class FPInfo implements Comparable<FPInfo> {
-	public String FPStr;
+	public TreeSet<Integer> ItemSet;
 	public int Frequency;
 	public ArrayList<Integer> MatchingList;
 
-	public FPInfo(String fPStr, int frequency) {
+	public FPInfo(TreeSet<Integer> fPStr, int frequency) {
 		super();
-		FPStr = fPStr;
+		ItemSet = fPStr;
 		MatchingList = new ArrayList<Integer>();
 		Frequency = frequency;
 	}
 
 	@Override
 	public String toString() {
-		return "FPInfo [FPStr=" + FPStr + ", Frequency=" + Frequency + ", MatchingList=" + MatchingList + "]";
+		return "FPInfo [FPStr=" + ItemSet + ", Frequency=" + Frequency
+				+ ", MatchingList=" + MatchingList + "]";
 	}
 
 	@Override
 	public int compareTo(FPInfo o) {
-		return FPStr.compareTo(FPStr);
+		return ItemSet.toString().compareTo(o.ItemSet.toString());
 	}
 
 	public boolean isSubFP(FPInfo o) {
-		if (this.FPStr.contains(o.FPStr) && ((o.Frequency * 1.0 / this.Frequency) < 1.5)) {
+		if (this.ItemSet.containsAll(o.ItemSet)
+				&& ((o.Frequency * 1.0 / this.Frequency) < 1.1)) {
 			return true;
 		}
 		return false;
+	}
+
+	public String getFPStr() {
+		String str = "";
+		Iterator<Integer> it = this.ItemSet.iterator();
+		while (it.hasNext()) {
+			str += it.next() + " ";
+		}
+		return str.trim();
+	}
+
+	public int[] getItemArr() {
+		int[] arr = new int[this.ItemSet.size()];
+		Iterator<Integer> it = this.ItemSet.iterator();
+		int i = 0;
+		while (it.hasNext()) {
+			arr[i] = it.next();
+			i++;
+		}
+		return arr;
 	}
 }
